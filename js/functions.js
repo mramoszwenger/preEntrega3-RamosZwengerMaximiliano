e86c367ae1e2b7e0244a80dad5d2839350825a99
Maximiliano Ramos Zwenger <mramoszwenger@gmail.com>
2023-09-18 20:36:47 -0300

    preEntrega3-v3.1.1

--- js/functions.js
+++ js/functions.js
@@ -4,6 +4,7 @@ const correo = document.querySelector('#correo');
 const servicio = document.querySelector('#servicio');
 const unidades = document.querySelector('#unidades');
 const inputs = document.querySelectorAll('.form-control');
+const botonPresupuesto = document.querySelector('#botonPresupuesto');
 
 let validarForm = false;
 
@@ -14,7 +15,7 @@ const solNube = 8946;
 const consTecnologica = 5000;
 const porcentajeIva = 0.21;
 
-// Funciones
+// Funcion Cotizador Rapido
 
 function calcularServicio() {
     const usuarioValor = usuario.value;
@@ -22,16 +23,50 @@ function calcularServicio() {
     const servicioValor = servicio.value;
     const unidadesValor = unidades.value;
 
-    const costoServicio = (Math.round(unidadesValor*5000).toFixed(2).replace('.', ','));
+    let costoServicio = 0;
 
-    console.log(usuarioValor, correoValor, servicioValor, unidadesValor, costoServicio);
+    switch (servicioValor) {
+        case "admSistemas":
+            costoServicio = unidadesValor * admSistemas;
+            break;
+        case "solNube":
+            costoServicio = unidadesValor * solNube;
+            break;
+        case "consTecnologica":
+            costoServicio = unidadesValor * consTecnologica;
+            break;
+        default:
+            costoServicio = 0;
+    }
+
+    const costoConIva = ((costoServicio * porcentajeIva) + costoServicio);
+    console.log("Usuario:", usuarioValor);
+    console.log("Correo:", correoValor);
+    console.log("Servicio:", servicioValor);
+    console.log("Unidades:", unidadesValor);
+    console.log("Costo del servicio (sin IVA): $" + costoServicio.toFixed(2));
+    console.log("Costo del servicio (con IVA): $" + costoConIva.toFixed(2).replace('.', ','));
 }
+
 
 formularioCotizaciones.addEventListener('submit', (e) => {
     e.preventDefault();
     calcularServicio();
     console.log(e);
 });
+
+// Solicutud Presupuesto a Medida
+
+botonPresupuesto.addEventListener('click', () => {
+    const email = 'info@errezetasoluciones.com.ar';
+    const usuarioValor = usuario.value;
+    const subject = `Solicitud de presupuesto a medida de ${usuarioValor}`;
+    const body = 'Hola, me gustaría solicitar un presupuesto a medida para nuestra empresa.'; 
+    const mailtoLink = `mailto:${email}?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`;
+  
+    window.location.href = mailtoLink;
+  });
+  
 
 inputs.forEach(input => {
     input.addEventListener('input', (e)=> {
